14a20193ab3fabcc9144b1c464125b68884e5207
Amadeo Bon <amadeosph@outlook.com>
2025-12-09 10:10:08 -0300

    Fix config.py

--- core/config.py
+++ core/config.py
@@ -2,22 +2,25 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    # BINANCE
+    # === BINANCE ===
     BINANCE_API_KEY: str | None = None
     BINANCE_API_SECRET: str | None = None
 
-    # POLYGON
+    # === POLYGON ===
     POLYGON_API_KEY: str | None = None
 
-    # ALPHA VANTAGE
-    ALPHA_VANTAGE_API_KEY: str | None = None
+    # === ALPHA VANTAGE ===
+    ALPHAVANTAGE_API_KEY: str | None = None
 
-    # TRADINGVIEW
+    # === TRADINGVIEW ===
     TV_USERNAME: str | None = None
     TV_PASSWORD: str | None = None
 
-    # OTHER
+    # === OTHER ===
     SECRET_KEY: str | None = None
+
+    # === CORS ===
+    ALLOWED_ORIGINS: list[str] = ["*"]
 
     class Config:
         env_file = ".env"
